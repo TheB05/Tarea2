234a1fe978fecb5a94fe294446a578cbea925fc7
TheB05 <branderbaez02@gmail.com>
2024-09-23 20:24:20 -0400

    Fixed the menu.

--- obj/Debug/net8.0/Tarea2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tarea2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc3b5f5b1267839cf4f04141bc96024c013fd68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
